bf23e1de62805aac8583879bdcb5e4c04d9f48e6
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2023-12-26 15:04:49 +0530

    pass of function

---
+++ pass.py
@@ -0,0 +1,6 @@
+#1. Function defination is one time process
+def hello():
+    # Pass statement is mandatory in empty space
+    pass
+
+
---
+++ vishal.py
@@ -0,0 +1,11 @@
+#1. Function defination is one time process
+def vishal(x):
+    # Pass statement is mandatory in empty function 
+    pass
+
+def anil():
+    print('Hello')
+    pass
+
+
+
